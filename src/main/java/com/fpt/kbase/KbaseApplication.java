545a2f7c3785551047f9655a3556b29b6f5a3ba8
truonggiangacc01-pixel <truonggiangacc01@gmail.com>
2026-09-19 14:45:24 +0700

    Add configuration setyp content

---
+++ src/main/java/com/fpt/kbase/KbaseApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.kbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbaseApplication.class, args);
+	}
+
+}
